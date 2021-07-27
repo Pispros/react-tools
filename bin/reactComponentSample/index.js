2007539a49e73d81485e0c24440b4176470c1665
ninjamer <pissanprosperr@gmail.com>
2021-07-27 02:16:16 +0000

    Async Redux

--- bin/reactComponentSample/index.js
+++ bin/reactComponentSample/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import reducer from './reducers'
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk));
 
 
 ReactDOM.render(
